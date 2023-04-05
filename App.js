7b990d90899f5eb6d75e60b783ef086b0ddde8a6
TajAlasfiyaa <tajalasfiyaa@gmail.com>
2023-04-05 19:08:28 +0200

    inisial commit

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Image,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  Alert,
+  View,
+  Dimensions,
+  Text,
+  useWindowDimensions,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="border-l-blue-500" style={styles.container}>
+      <View style={styles.main} />
+      <View style={{ flex: 1 / 3, flexDirection: "row" }}>
+        <
+        <View
+          style={{
+            backgroundColor: "black",
+            height: 140,
+            width: 140,
+          }}
+        ></View>
+        <View
+          style={{
+            backgroundColor: "green",
+            height: 140,
+            width: 140,
+          }}
+        ></View>
+        <View
+          style={{
+            backgroundColor: "yellow",
+            height: 140,
+            width: 140,
+          }}
+        ></View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    paddingTop: Platform.OS == "android" ? StatusBar.currentHeight : 0,
+  },
+  main: {
+    backgroundColor: "dodgerblue",
+    width: "100%",
+    height: "90%",
   },
 });
